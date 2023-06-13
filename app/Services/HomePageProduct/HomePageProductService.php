25d57ae2eb291f080d6cf29de81de6a5fa1dafbf
amirhamza05 <amir.hamza@streamstech.com>
2023-06-14 05:37:08 +0600

    Update Home Page Product

--- app/Services/HomePageProduct/HomePageProductService.php
+++ app/Services/HomePageProduct/HomePageProductService.php
@@ -2,6 +2,8 @@
 
 namespace App\Services\HomePageProduct;
 
+use App\Models\Category;
+use Illuminate\Pagination\LengthAwarePaginator;
 use App\Models\HomePageProduct;
 use App\Models\Product;
 use Illuminate\Support\Facades\DB;
@@ -12,7 +14,10 @@ class HomePageProductService
     {
         $mxSerialNo = HomePageProduct::max('serial_no') + 1;
 
-        $products = Product::with(['imageTable'])->where(['status' => 'publish', 'has_stock' => true])->leftJoin('home_page_products', function ($join) {
+        $products = [];
+        $productsId = [];
+
+        $topProducts = Product::with(['imageTable'])->where(['status' => 'publish', 'has_stock' => true])->leftJoin('home_page_products', function ($join) {
             $join->on('products.id', '=', 'home_page_products.product_id');
         })->leftJoin($this->getOrderSaleQueryTable(), function ($join) {
             $join->on('products.id', '=', 'sale_count_table.product_id');
@@ -20,9 +25,45 @@ class HomePageProductService
             'products.*',
             DB::raw('IFNULL(serial_no, ' . $mxSerialNo . ') as serial_no'),
             DB::raw('IFNULL(total_sale , 0) as total_sale')
-        ])->orderByRaw('serial_no asc, total_sale desc')->paginate(6);
+        ])->orderByRaw('serial_no asc, total_sale desc')->limit(18)->get();
 
-        return $products;
+        foreach ($topProducts as $key => $product) {
+            if ($key == 0) $product->category_name = "জনপ্রিয় পণ্য";
+            $products[] = $product;
+            $productsId[] = $product->id;
+        }
+
+        $categories = Category::with(['products' => function ($query) {
+            $query->where('has_stock', true);
+            $query->where('status', 'publish');
+            $query->orderBy('serial', 'asc');
+        }, 'products.imageTable'])->get();
+        foreach ($categories as $category) {
+            $categoryProducts = $category->products;
+            foreach ($categoryProducts as $key => $product) {
+                if ($key == 0) $product->category_name = $category->name;
+                $products[] = $product;
+                $productsId[] = $product->id;
+            }
+        }
+
+        $allProducts = Product::whereNotIn('id', $productsId)->where(['has_stock' => true, 'status' => 'publish'])->with(['imageTable'])->get();
+        foreach ($allProducts as $key => $product) {
+            if ($key == 0) $product->category_name = 'ক্যাটাগরি বিহীন';
+            $products[] = $product;
+        }
+
+        $products = collect($products);
+
+        return new LengthAwarePaginator(
+            $products->forPage(request()->page, 12),
+            $products->count(),
+            12,
+            request()->page,
+            ['path' => request()->url(), 'query' => request()->query()]
+        );
+
+        // return collect($products)->paginate();
     }
 
     public function getOrderSaleQueryTable()
--- resources/views/store/product/single_product_page.blade.php
+++ resources/views/store/product/single_product_page.blade.php
@@ -1,9 +1,14 @@
-
 @foreach ($products as $product)
+    @if ($product->category_name != '')
+        <div class="col-md-12">
+            <h4>{{ $product->category_name }}</h4>
+            <hr>
+        </div>
+    @endif
     <div class="col-md-2 col-sm-6 col-lg-2 col-6">
         {{-- @include('store.product.single_product', ['product' => $product]) --}}
         @livewire('shop-product', ['product' => $product])
-    </div>    
+    </div>
 @endforeach
 
 
