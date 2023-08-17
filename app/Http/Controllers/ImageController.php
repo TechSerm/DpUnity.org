9c252e22337c366b9e29a0e9e730d676de2fbe6e
amirhamza05 <amir.hamza@streamstech.com>
2023-08-17 23:36:51 +0600

    Reduce file size

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -16,16 +16,32 @@ class ImageController extends Controller
             abort(404);
         }
 
-        $image = Image::make($basePath);
+        // $image = Image::make($basePath);
 
-        // Get the width and height parameters from the URL
-        $width = request()->get('width', $image->width());
-        $height = request()->get('height', $image->height());
+        // // Get the width and height parameters from the URL
+        // $width = request()->get('width', $image->width());
+        // $height = request()->get('height', $image->height());
 
-        // Resize the image
-        $image->resize($width, $height);
+        // // Resize the image
+        // $image->resize($width, $height);
 
         // Return the resized image as a response
-        return $image->response();
+        return $this->getImage($basePath, $filename);
+    }
+
+    private function getImage($basePath, $filename)
+    {
+        $cacheKey = 'resized_' . $filename . '_' . request('width', 'default') . '_' . request('height', 'default');
+
+        return cache()->remember($cacheKey, now()->addYear(), function () use ($basePath, $filename) {
+            $image = Image::make($basePath);
+
+            $width = request()->get('width', $image->width());
+            $height = request()->get('height', $image->height());
+
+            $image->resize($width, $height);
+
+            return $image->response();
+        });
     }
 }
