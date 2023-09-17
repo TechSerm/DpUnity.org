c6494ce018ce536d8e902f4bce33a86069da0f52
amirhamza05 <amir.hamza@streamstech.com>
2023-09-17 08:37:59 +0600

    Update Print Page

--- resources/views/invoice/print.blade.php
+++ resources/views/invoice/print.blade.php
@@ -91,9 +91,7 @@
             }
         }
 
-        .image-container {
-
-        }
+        .image-container {}
 
         .watermark {
             position: absolute;
@@ -101,13 +99,123 @@
             margin-top: 15%;
             transform: rotate(310deg);
         }
-        .infoBox{
+
+        .infoBox {
             border: 1px solid #dbdbdb;
             color: #474747;
             font-weight: bold;
             padding: 5px 10px 5px 10px;
             margin-right: 5px;
             border-radius: 5px;
+        }
+
+        .vendor-area {
+            border-top: 1px dashed #000000;
+            margin-top: 30px;
+            padding-top: 20px;
+            font-size: 12px;
+
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            text-align: center;
+        }
+    
+
+        /* Style the individual box */
+        .vendor-area .box {
+            border: 1px solid #ccc;
+            flex: 1;
+        }
+
+        .vendor-area .body {
+            padding: 0px 0px 5px 0px;
+            text-align: center;
+        }
+
+        /* Style the header and body of the box */
+        .vendor-area .header {
+            font-weight: bold;
+            text-align: center;
+            padding: 5px 0px 0px 0px;
+        }
+
+        .vendor-area .store-name {
+            text-align: center;
+        }
+
+        .card {
+            float: left;
+            border: 2px solid #2E363F;
+            width: 214px;
+            min-height: 100px;
+            margin-right: 15px;
+            margin-top: 15px;
+            font-size: 12px;
+            overflow: hidden;
+            border-radius: 3px;
+        }
+
+        .card .header {
+            text-align: center;
+            padding: 5px 2px 5px 2px;
+            font-weight: bold;
+            color: #ffffff;
+            background: #474747;
+        }
+
+        .card .label {
+            background: #474747;
+            color: #ffffff;
+            width: 150px;
+            font-weight: bold;
+            text-align: center;
+            padding-bottom: 2px;
+            border-radius: 0px 0px 10px 10px;
+        }
+
+        .card .td1 {
+            border: 1px solid #eeeeee;
+            background: #eeeeee;
+            text-align: right;
+            padding: 2px 2px 2px 2px;
+            font-weight: bold;
+            font-size: 12px;
+        }
+
+        .card .td2 {
+            border: 1px solid #eeeeee;
+            background: #f5f5f5;
+            padding: 2px 2px 2px 2px;
+            font-size: 12px;
+        }
+
+        .card .body {
+            padding: 5px 5px 3px 5px;
+
+        }
+
+        .card .coderoj_t {
+            position: absolute;
+            margin-top: -90px;
+            z-index: 100 !important;
+
+            opacity: 0.2;
+
+        }
+
+        .card .credintials_label {
+            background-color: #474747;
+            font-weight: bold;
+            color: #ffffff;
+            padding: 2px 5px 2px 5px;
+            font-size: 13px;
+            border-radius: 5px;
+        }
+
+        .card .credintials_label a {
+            color: #ffffff;
+            text-decoration: none;
         }
     </style>
 </head>
@@ -119,12 +227,14 @@
                 <img src="{{ asset('assets/img/bibisena_logo.png') }}" alt="">
             </div>
             <div style="float: right; width: 55%;  color: #555555; font-size: 14px">
-                আপনার নিত্য প্রয়োজনীয় যেকোন পন্য যেমনঃ চাল, ডাল, তেল, গ্যাস সিলিন্ডার, সবজি, ফল ইত্যাদি পন্য বাজার মূল্যে ঘরে বসে ৩০-৪৫ মিনিটের মধ্যে পেতে অর্ডার করুন বিবিসেনা অনলাইন শপে।
+                আপনার নিত্য প্রয়োজনীয় যেকোন পন্য যেমনঃ চাল, ডাল, তেল, গ্যাস সিলিন্ডার, সবজি, ফল ইত্যাদি পন্য বাজার
+                মূল্যে ঘরে বসে ৩০-৪৫ মিনিটের মধ্যে পেতে অর্ডার করুন বিবিসেনা অনলাইন শপে।
                 <div style="margin-top: 5px;text-align: center; padding-top: 10px;">
-                    <span class="infoBox">Bibisena.Com</span> <span class="infoBox"><i class="fa fa-phone"></i> {{ env('STORE_MOBILE_NUMBER') }}</span> 
+                    <span class="infoBox">Bibisena.Com</span> <span class="infoBox"><i class="fa fa-phone"></i>
+                        {{ env('STORE_MOBILE_NUMBER') }}</span>
                 </div>
             </div>
-            
+
             <div style="clear: both;"></div>
         </div>
         {{-- <div class="image-container">
@@ -136,9 +246,9 @@
         </div>
 
         <div class="invoice-details" style="float: right;">
-            <div style="font-size: 30px;">অর্ডার নাম্বার: {{ bnConvert()->number($order->id) }}</div>
+            <div style="font-size: 30px;">অর্ডার নাম্বারঃ {{ bnConvert()->number($order->id) }}</div>
             <div style="font-size: 14px;">
-                অর্ডারটি করা হয়েছে: {{ bnConvert()->date($order->created_at->format('d M Y, h:i a')) }}
+                অর্ডারটি করা হয়েছেঃ {{ bnConvert()->date($order->created_at->format('d M Y, h:i a')) }}
             </div>
         </div>
         <div style="clear: both;"></div>
@@ -171,15 +281,15 @@
             </tbody>
             <tfoot>
                 <tr>
-                    <td colspan="4" class="total-td">পণ্যের মূল্য:</td>
+                    <td colspan="4" class="total-td">পণ্যের মূল্যঃ</td>
                     <td colspan="1">{{ bnConvert()->number($order->subtotal) }} টাকা</td>
                 </tr>
                 <tr>
-                    <td colspan="4" class="total-td">ডেলিভারি ফি:</td>
+                    <td colspan="4" class="total-td">ডেলিভারি ফিঃ</td>
                     <td colspan="1">{{ bnConvert()->number($order->delivery_fee) }} টাকা</td>
                 </tr>
                 <tr style="font-weight: bold;">
-                    <td colspan="4" class="total-td">সর্বমোট মূল্য:</td>
+                    <td colspan="4" class="total-td">সর্বমোট মূল্যঃ</td>
                     <td colspan="1">{{ bnConvert()->number($order->total) }} টাকা</td>
                 </tr>
             </tfoot>
@@ -195,6 +305,8 @@
         যোগাযোগ করুণ। আমাদের হটলাইন নাম্বার {{ env('STORE_MOBILE_NUMBER') }}। আপনার পণ্যগুলো যদি খারাপ, মেয়াদ শেষ,
         ওজণে কম অথবা কোনো পণ্য যদি অনুপস্থিত থাকে আমাদের জানান। আমরা আপনার পণ্য গুলো আবার পরিবর্তন করে দিব।
     </div>
+    @include('invoice.vendor_area')
+
 </body>
 
 </html>
---
+++ resources/views/invoice/vendor_area.blade.php
@@ -0,0 +1,85 @@
+
+<div class="vendor-area">
+
+    <div class="card" style="border-color: red">
+        <div class="header" style="background: red; ">
+            বিবিসেনা অনলাইন শপ
+        </div>
+        <center>
+            <div class="label" style="background: red;">
+                অর্ডার নাম্বারঃ {{ bnConvert()->number($order->id) }}
+            </div>
+        </center>
+
+        <div class="body">
+            <table style="width: 100%;z-index: 800">
+                <tr>
+                    <td class="td2" colspan="2">
+                        <b>ক্যাশে জমা</b>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="td1">তারিখঃ</td>
+                    <td class="td2">
+                        {{ bnConvert()->date($order->created_at->format('d M Y')) }}
+                    </td>
+                </tr>
+                
+                <tr>
+                    <td class="td1">মোটঃ</td>
+                    <td class="td2"><b>{{ bnConvert()->number($order->products_profit + $order->delivery_fee) }}</b> টাকা</td>
+                </tr>
+            </table>
+            <div class="coderoj_t">
+                <img style="margin-left: 10px;margin-top: 5px;" height="60" width="180"
+                    src="{{ asset('assets/img/bibisena_logo.png') }}">
+            </div>
+        </div>
+    </div>
+
+    @php
+        $vendors = $order
+            ->vendors()
+            ->with('user')
+            ->orderBy('wholesale_total', 'desc')
+            ->get();
+    @endphp
+
+    @foreach ($vendors as $vendor)
+        <div class="card" ng-repeat="user in users" style="border-color: green">
+            <div class="header" style="background: green">
+                বিবিসেনা অনলাইন শপ
+            </div>
+            <center>
+                <div class="label" style="background: green">
+                    অর্ডার নাম্বারঃ {{ bnConvert()->number($order->id) }}
+                </div>
+            </center>
+
+            <div class="body">
+                <table style="width: 100%;z-index: 800">
+                    <tr>
+                        <td class="td1">তারিখঃ</td>
+                        <td class="td2">
+                            {{ bnConvert()->date($order->created_at->format('d M Y')) }}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="td1">বিক্রেতাঃ</td>
+                        <td class="td2">
+                            {{ $vendor->user->name }}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="td1">মোটঃ</td>
+                        <td class="td2"><b>{{ bnConvert()->number($vendor->wholesale_total) }}</b> টাকা</td>
+                    </tr>
+                </table>
+                <div class="coderoj_t">
+                    <img style="margin-left: 10px;margin-top: 25px;" height="60" width="180"
+                        src="{{ asset('assets/img/bibisena_logo.png') }}">
+                </div>
+            </div>
+        </div>
+    @endforeach
+</div>
